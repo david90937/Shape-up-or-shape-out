0ebfe2e1255022c5492acf66e27012c22d68cc82
David Robinette <drobin4@gmail.com>
2021-09-24 19:29:29 -0500

    added remainder of basic features

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-let circleBtn = document.getElementById('circle-btn');
 let shapeContainer = document.querySelector('.shape-container');
+let circleBtn = document.getElementById('circle-btn');
+let triangleBtn = document.getElementById('triangle-btn');
+let rectangleBtn = document.getElementById('rectangle-btn');
+let squareBtn = document.getElementById('square-btn');
 let userInputCircle = document.getElementById('circle-radius');
-let userInput = 0;
-let circleVal = Math.floor(Math.random() * (600 - (2 * userInputCircle.value)))
+let userInputTriangle = document.getElementById('triangle-height')
+let userInputRectangleHeight = document.getElementById('rectangle-height');
+let userInputRectangleWidth = document.getElementById('rectangle-width');
+let userInputSquare = document.getElementById('square-side');
+// let circleRadius = userInputCircle.value;
+// let triangleHeight = userInputTriangle.value
+// let rectangleHeight = userInputRectangleHeight.value
+// let rectangleWidth = userInputRectangleWidth.value
+// let squareSide = userInputSquare.value
 
 class Shape {
-    constructor(width, height){
+    constructor(height, width){
         this.div = document.createElement('div');
         this.name;
         this.width = `Width: ${width}`;
         this.height = `Height: ${height}`;
-        this.x = Math.floor(Math.random() * (600 - (2 * userInputCircle.value)));
-        this.y = Math.floor(Math.random() * (600 - (2 * userInputCircle.value)));
+        this.x = Math.floor(Math.random() * (600 - (0.5 * (height + width))));
+        this.y = Math.floor(Math.random() * (600 - (0.5 * (height + width))));
         this.div.style.height = `${height}px`;
         this.div.style.width = `${width}px`;
         this.div.addEventListener('click', () => {
@@ -45,35 +55,95 @@ class Circle extends Shape {
         this.radius = `Radius: ${radius}`;
         this.perimeter = `Perimeter: ${Math.floor((2* 3.1415) * radius)}`;
         this.area = `Area: ${Math.floor(3.1415 * (radius*radius))}`;
-
         this.div.classList.add('circle', 'shape');
+        this.div.style.left = `${this.x}px`;
+        this.div.style.top = `${this.y}px`;
+    }       
+}
+
+class Triangle extends Shape {
+    constructor(height){
+        super(1*height, 1*height) // added 1* due to type conversion needs.
+        this.hypotenuse = Math.sqrt((height * height) + (height * height));
+        this.name = 'Name: Triangle'
+        this.area = `Area: ${0.5 * height * height}`;
+        this.radius = `Radius: N/A`;
+        this.perimeter = `Perimeter: ${Math.floor(this.hypotenuse) + (2 * height)}`;
+        this.div.classList.add('shape');
+        this.div.style.left = `${this.x}px`;
+        this.div.style.top = `${this.y}px`;
+        this.div.style.borderBottom =  `${height}px solid yellow`
+        this.div.style.borderRight=  `${height}px solid transparent`
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(height, width){
+        super(1*height, 1*width) // added 1* due to type conversion needs.
+        this.name = 'Name: Rectangle'
+        this.area = `Area: ${width * height}`;
+        this.radius = `Radius: N/A`;
+        this.perimeter = `Perimeter: ${(2 * width) + (2 * height)}`;
+        this.x = Math.floor(Math.random() * (600 - (0.5 * (2 * width))));
+        this.y = Math.floor(Math.random() * (600 - (0.5 * (2 * height))));
+        this.div.classList.add('rectangle', 'shape');
         this.div.style.left = `${this.x}px`;
         this.div.style.top = `${this.y}px`;
     }
 }
 
-class Triangle extends Shape {
-    constructor(height){
-        super()
-    }
-}
-
-class Rectangle extends Shape {
-    constructor(width, height){
-        super()
-    }
-}
-
 class Square extends Shape {
     constructor(sideLength){
-        super()
+        super(1*sideLength, 1*sideLength) // added 1* due to type conversion needs.
+        this.name = 'Name: Square'
+        this.area = `Area: ${sideLength * sideLength}`;
+        this.radius = `Radius: N/A`;
+        this.perimeter = `Perimeter: ${4 * sideLength}`;
+        this.div.classList.add('square', 'shape');
+        this.div.style.left = `${this.x}px`;
+        this.div.style.top = `${this.y}px`;
     }
 }
 
 circleBtn.addEventListener('click', () => {
+    if(userInputCircle.value > 300){
+        alert('Please enter a value of 300px or less')
+        userInputCircle.value = "";
+        return;
+    }
     let circle = new Circle(userInputCircle.value);
     shapeContainer.append(circle.div);
-    console.log(circle.x)
-    console.log(circle.y)
-    console.log(circle.perimeter);
+})
+
+triangleBtn.addEventListener('click', () => {
+    if(userInputTriangle.value > 600){
+        alert('Please enter a value of 600px or less')
+        userInputTriangle.value = "";
+        return;
+    }
+    let triangle = new Triangle(userInputTriangle.value);
+    shapeContainer.append(triangle.div);
+})
+rectangleBtn.addEventListener('click', () => {
+    if(userInputRectangleHeight.value > 600){
+        alert('Please enter a value of 600px or less')
+        userInputRectangleHeight.value = "";
+        return;
+    }
+    if (userInputRectangleWidth,value > 600){
+        alert('Please enter a value of 600px or less')
+        userInputRectangleHeight.value = "";
+        return;
+    }
+    let rectangle = new Rectangle(userInputRectangleHeight.value, userInputRectangleWidth.value);
+    shapeContainer.append(rectangle.div);
+})
+squareBtn.addEventListener('click', () => {
+    if(userInputSquare.value > 600){
+        alert('Please enter a value of 600px or less')
+        userInputSquare.value = "";
+        return;
+    }
+    let square = new Square(userInputSquare.value);
+    shapeContainer.append(square.div);
 })
